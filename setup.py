830500db3889f9a52230146407143907bfd073a3
Stephan Kaminsky <thaumicmekanism@gmail.com>
2020-08-08 21:36:55 -0700

    Updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="examtool",
-    version="2.1.0",
+    version="2.0.0",
     author="Rahul Arya",
     author_email="rahularya@berkeley.edu",
     long_description=readme,
